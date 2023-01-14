c5d96ffa71e5dca1f28a4bc792323b2b5960fa93
Hayden Leete <haydenleete@gmail.com>
2023-01-15 10:16:13 +1300

    Add struts, placeholder laser, and camera zoom

--- scenes/Entities/Parts/Part.gd
+++ scenes/Entities/Parts/Part.gd
@@ -4,3 +4,5 @@ class_name Part
 export var name : String
 export(String, MULTILINE) var description : String
 export var icon : Texture
+export var power_draw : int
+export(PackedScene) var shootemup_scene
