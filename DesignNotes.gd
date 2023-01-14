921cd9f91a4bdbf8dfdf787fd6de48b5371c750f
plexsoup <plexsoup@gmail.com>
2023-01-14 00:43:51 -0800

    accessibility menus
    relocated return button. Modified hboxes/vboxes to more logical placements.

---
+++ DesignNotes.gd
@@ -0,0 +1,34 @@
+"""
+DESIGN NOTES:
+	Repo: https://github.com/plexsoup/GWJ53
+	Issues: https://github.com/plexsoup/GWJ53/issues
+	Project: https://github.com/users/plexsoup/projects/2/views/1
+	
+	
+GWJ 53: Mech Repair Simulator
+1960's - 1970's era custom Muscle Mechs
+
+Player repairs mechas.
+- adds rims, paint and spoilers
+
+Then fights the mech.
+
+Spoils from the fight go into the player's inventory
+for upgrading or replacing mech.
+
+Whenever the player adopts a new chassis, the old mech gets tossed.
+Later, the player will have to fight all those old mechs.
+
+MVP:
+	Build Mech
+	Fight Mechs - top down shooter
+	
+Nice to Have:
+	Story, reasons to fight, upgrades, payoff
+	
+
+
+"""
+
+extends Node
+
