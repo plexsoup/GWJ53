8177bee0da248d36664865dc3501944c3a18d254
plexsoup <plexsoup@gmail.com>
2023-01-16 03:35:34 -0800

    tweaking sizes

--- DesignNotes.gd
+++ DesignNotes.gd
@@ -4,7 +4,9 @@ DESIGN NOTES:
 	Issues: https://github.com/plexsoup/GWJ53/issues
 	Project: https://github.com/users/plexsoup/projects/2/views/1
 	Figma: https://www.figma.com/file/9D4oo1ATOVsIhFqvb4oXIB/Untitled?node-id=0%3A1&t=H3yvrpDP7pCNzjnp-1
-	
+	Design Doc: https://docs.google.com/presentation/d/1Jb92QlMrf8zeSP-lPKnFi0Qnvu1gNCSONkcWnr_ncO0/edit#slide=id.p
+
+
 GWJ 53: Mech Repair Simulator
 1960's - 1970's era custom Muscle Mechs
 
