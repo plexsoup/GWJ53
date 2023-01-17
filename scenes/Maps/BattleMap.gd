f4a35ecef9cfb47c3a6d16ab738a35082f9529f7
plexsoup <plexsoup@gmail.com>
2023-01-16 21:05:59 -0800

    adding win conditions

--- scenes/Maps/BattleMap.gd
+++ scenes/Maps/BattleMap.gd
@@ -1,21 +1,38 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var done_initial_spawns : bool = false
 
+enum States { SPAWNING, FIGHTING, PAUSED, FINISHED }
+var state = States.SPAWNING
+
+var num_spawners : int # count the $Spawners children
+var spawners_finished_spawning : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Global.current_scene = self
 	init(Global.player)
 
+	num_spawners = $Spawners.get_child_count()
+	for spawner in $Spawners.get_children():
+		if spawner.has_signal("finihsed"):
+			spawner.connect("finished", self, "_on_spawner_finished")
+
 func init(playerScene): # called by MechBuilderTest when user presses finished
 	playerScene.set_scale(Vector2(0.33, 0.33))
 	$YSort/Entities.add_child(playerScene)
 	
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_spawner_finished():
+	spawners_finished_spawning += 1
+	if spawners_finished_spawning == num_spawners:
+		state = States.FIGHTING
+		done_initial_spawns = true
+		
+func _on_player_won():
+	print("Congratulations!")
+	if has_node("HUD"):
+		if $HUD.has_method("_on_player_won"):
+			$HUD._on_player_won()
+
