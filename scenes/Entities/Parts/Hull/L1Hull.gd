84eb5fc930a2f97530925040a99a6d1956ae764d
plexsoup <plexsoup@gmail.com>
2023-01-19 03:05:41 -0800

    animations and stuff

---
+++ scenes/Entities/Parts/Hull/L1Hull.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var mech
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func init(myMech):
+	mech = myMech
+
+
+func _on_stopped_walking(_velocity):
+	$AnimationPlayer.play("idle")
+
+func _on_started_walking(_velocity):
+	$AnimationPlayer.play("walk")
