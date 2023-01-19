f645866124a33da3c2bd4de4ca25ac7b67f36135
Hayden Leete <haydenleete@gmail.com>
2023-01-20 00:47:18 +1300

    Add parts selling

--- scenes/MechBuilder/MechBuilder.gd
+++ scenes/MechBuilder/MechBuilder.gd
@@ -11,12 +11,15 @@ onready var part_buttons_hbox = $"%PartButtonsHBox"
 var part_buttons = []
 onready var money_label : Label = $"%MoneyLabel"
 onready var cursor : Sprite = $"%Cursor"
+onready var sell_button = $"%SellButton"
 var selected_part : Part
 var selected_part_button = null
 var can_place_part = false
 
 var strut_hints = []
 var building_parts = []
+var hull_part : BuildingPart
+var hovered_part : BuildingPart
 
 func _ready():
 	var _discard
@@ -24,7 +27,9 @@ func _ready():
 		add_default_mech()
 	else:
 		for inner_part in Global.persistent_mech.inner_parts:
-			inner_part = inner_part as MechStructure.MechStructurePart
+			add_building_part(inner_part.part)
+			if inner_part.part.type == Part.Type.HULL:
+				hull_part = building_parts[-1]
 	
 	for part in Global.parts_pool.values():
 		if part.type != Part.Type.HULL:
@@ -33,6 +38,7 @@ func _ready():
 
 func add_default_mech():
 	add_building_part(Global.parts_pool["L1Hull"])
+	hull_part = building_parts[-1]
 	add_building_part(Global.parts_pool["Legs"], Vector2(0, 150))
 	add_building_part(Global.parts_pool["LongRangeLaser"], Vector2(0, -150))
 	
@@ -43,6 +49,7 @@ func _update_money_display():
 
 
 func _on_part_button_pressed(part_button):
+	sell_button.pressed = false
 	selected_part = part_button.part
 	selected_part_button = part_button
 	cursor.texture = part_button.part.icon
@@ -65,6 +72,7 @@ func get_nearby_parts(position : Vector2) -> Array:
 	return nearby_parts
 
 func _process(_delta):
+	
 	cursor.global_position = get_global_mouse_position()
 	
 	var nearby_parts = get_nearby_parts(cursor.global_position)
@@ -98,7 +106,10 @@ func _process(_delta):
 
 func add_building_part(part : Part, position : Vector2 = Vector2.ZERO):
 	# Add part
-	var building_part = building_part_scene.instance()
+	var building_part : BuildingPart = building_part_scene.instance()
+	building_part.connect("mouse_entered", self, "_on_building_part_mouse_enter", [building_part])
+	building_part.connect("mouse_exited", self, "_on_building_part_mouse_exit", [building_part])
+	
 	building_part.part = part
 	$BuildingZone.add_child(building_part)
 	building_part.global_position = position
@@ -107,16 +118,72 @@ func add_building_part(part : Part, position : Vector2 = Vector2.ZERO):
 	# Add struts
 	var tween = create_tween().set_parallel()
 	for nearby_part in get_nearby_parts(building_part.global_position):
+		if nearby_part == building_part: continue
 		var strut = strut_scene.instance()
-		$BuildingZone.add_child(strut)
+		nearby_part.add_child(strut)
+		building_part.connect("fling", strut, "queue_free")
+		nearby_part.connect("fling", strut, "queue_free")
 		tween.tween_property(strut, "length", building_part.position.distance_to(nearby_part.position), 0.2)
-		strut.position = nearby_part.position
-		strut.look_at(building_part.position)
-		$BuildingZone.move_child(strut, 0)
+		strut.look_at(building_part.global_position)
+		strut.z_index = -10
+		
+		nearby_part.connected_parts.append(building_part)
+		building_part.connected_parts.append(nearby_part)
+	if building_part.connected_parts.size() == 0:
+		tween.kill()
 
+func _on_building_part_mouse_enter(building_part : BuildingPart):
+	if sell_button.pressed:
+		hovered_part = building_part
+		building_part.modulate = Color(1,1,1,0.4)
+
+func _on_building_part_mouse_exit(building_part : BuildingPart):
+	building_part.modulate = Color.white
+	if hovered_part == building_part:
+		hovered_part = null
+
+
+# Removes a building part from mech, and removes any building parts no longer
+# connected to the hull.
+# Returns an array of all parts removed this way
+func remove_building_part(building_part : BuildingPart) -> Array:
+	# remove connections
+	for connected_part in building_part.connected_parts:
+		connected_part.connected_parts.erase(building_part)
+	building_part.connected_parts.clear()
+	
+	# find all parts connected to hull
+	var parts_on_hull = [hull_part]
+	# and all the parts no longer connected to the hull
+	var remaining_parts = building_parts.duplicate()
+	var i = 0
+	while i < parts_on_hull.size():
+		var part : BuildingPart = parts_on_hull[i]
+		remaining_parts.erase(part)
+		for connected_part in part.connected_parts:
+			if connected_part in parts_on_hull : continue
+			parts_on_hull.append(connected_part)
+		i += 1
+	
+	for part in remaining_parts:
+		building_parts.erase(part)
+		part.fling()
+	
+	return remaining_parts
 
 func _unhandled_input(event):
 	if event.is_action_pressed("place_part"):
+		
+		if sell_button.pressed:
+			if hovered_part != null:
+				for removed_part in remove_building_part(hovered_part):
+					Global.money += removed_part.part.cost
+					$"%MoneyAnimation".stop()
+					$"%MoneyAnimation".play("GainMoney")
+				hovered_part = null
+				_update_money_display()
+			return
+		
 		if not can_place_part:
 			return
 		if selected_part.cost > Global.money:
@@ -170,3 +237,8 @@ func _on_FightButton_pressed():
 	Global.player = player
 	Global.persistent_mech = mech_structure
 	Global.stage_manager.start_next_battle(player)
+
+
+func _on_SellButton_pressed():
+	if hovered_part != null:
+		hovered_part.modulate = Color.white
