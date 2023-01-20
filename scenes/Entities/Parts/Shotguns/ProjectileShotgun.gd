3249a09bcf4db1975a6aaa90074f5ef24ac30248
Extrevorted <extrevorted@gmail.com>
2023-01-19 23:21:14 -0800

    Add Shotgun

---
+++ scenes/Entities/Parts/Shotguns/ProjectileShotgun.gd
@@ -0,0 +1,157 @@
+"""
+Basic missile launcher.
+Might track the mouse, or maybe autotargets.
+
+Spawns a projectile which deals damage on impact.
+
+To meet accessibility requirements:
+	Every weapon needs a mouselook targetted attack and an autotarget.
+
+
+"""
+
+extends Node2D
+
+var mech
+export var projectile_range : float = 200.0
+export var damage : float = 500.0
+export (Global.damage_types) var damage_type : int = Global.damage_types.LASER
+export var line_of_sight : bool = false
+
+export var shots_per_burst : int = 1
+export var burst_delay : float = 0.5
+export var reload_time : float = 1.0
+
+var shots_left : int = shots_per_burst
+
+
+
+export var projectile : PackedScene
+
+
+enum States { RELOADING, COCKING }
+var State = States.RELOADING
+
+var target_locked : Node2D # probably a kinematic body
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	$ReloadTimer.start()
+	$ProjectileShotgunSprite/MuzzleFlash.visible = false
+
+	$CockDurationTimer.set_wait_time(burst_delay)
+	$ReloadTimer.set_wait_time(reload_time)
+
+func init(myMech):
+	mech = myMech
+
+
+func scene_finished():
+	if Global.current_scene.State == Global.current_scene.States.FINISHED:
+		return true
+
+
+func shoot():
+	if scene_finished():
+		return
+		
+	# WIP we should ask the target acquisition system for a target
+	if mech != null and mech.State == mech.States.READY:
+		shots_left -= 1
+		if shots_left == 0:
+			State = States.RELOADING
+			$ReloadTimer.start()
+		else:
+			State = States.COCKING
+			$CockDurationTimer.start()
+		make_noise()
+		flash_muzzle()
+		spawn_projectile()
+
+
+
+
+func spawn_projectile():
+	var newProjectile1 = projectile.instance()
+	var newProjectile2 = projectile.instance()
+	var newProjectile3 = projectile.instance()
+	var newProjectile4 = projectile.instance()
+	var newProjectile5 = projectile.instance()
+	var targetPos = mech.targetting_cursor.global_position
+	newProjectile1.init(mech, damage, damage_type, projectile_range, line_of_sight, targetPos)
+	newProjectile2.init(mech, damage, damage_type, projectile_range, line_of_sight, targetPos)
+	newProjectile3.init(mech, damage, damage_type, projectile_range, line_of_sight, targetPos)
+	newProjectile4.init(mech, damage, damage_type, projectile_range, line_of_sight, targetPos)
+	newProjectile5.init(mech, damage, damage_type, projectile_range, line_of_sight, targetPos)
+
+	if Global.current_scene != null:
+#		if Global.current_scene.has_node("YSort/Projectiles"):
+#			Global.current_scene.get_node("Ysort/Projectiles").add_child(newProjectile)
+		Global.current_scene.add_child(newProjectile1)
+		Global.current_scene.add_child(newProjectile2)
+		Global.current_scene.add_child(newProjectile3)
+		Global.current_scene.add_child(newProjectile4)
+		Global.current_scene.add_child(newProjectile5)
+		newProjectile1.global_position = $ProjectileShotgunSprite/MuzzleLocation.global_position
+		newProjectile1.global_rotation = $ProjectileShotgunSprite.global_rotation
+		newProjectile2.global_position = $ProjectileShotgunSprite/MuzzleLocation2.global_position
+		newProjectile2.global_rotation = $ProjectileShotgunSprite/MuzzleLocation2.global_rotation
+		newProjectile3.global_position = $ProjectileShotgunSprite/MuzzleLocation3.global_position
+		newProjectile3.global_rotation = $ProjectileShotgunSprite/MuzzleLocation3.global_rotation
+		newProjectile4.global_position = $ProjectileShotgunSprite/MuzzleLocation4.global_position
+		newProjectile4.global_rotation = $ProjectileShotgunSprite/MuzzleLocation4.global_rotation
+		newProjectile5.global_position = $ProjectileShotgunSprite/MuzzleLocation5.global_position
+		newProjectile5.global_rotation = $ProjectileShotgunSprite/MuzzleLocation5.global_rotation
+
+
+func make_noise():
+	$ShootNoise.set_pitch_scale(rand_range(0.8, 1.2))
+	$ShootNoise.play()
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	aim(delta)
+	
+
+func aim(_delta):
+
+	#if Global.player_cursor != null:
+	if mech.targetting_cursor != null:
+		var myPos = self.global_position
+		var cursorPos = mech.targetting_cursor.get_global_position() # note, cursor might be on autopilot depending on Global.auto_targetting
+		var targetPos = cursorPos
+		if myPos.distance_squared_to(cursorPos) > projectile_range * projectile_range:
+			targetPos = myPos.direction_to(cursorPos)*projectile_range
+		else:
+			targetPos = cursorPos - myPos
+			
+		$ProjectileShotgunSprite.look_at(targetPos + myPos)
+		
+
+
+func flash_muzzle():
+	if has_node("AnimationPlayer"):
+		if $AnimationPlayer.has_animation("flash"):
+			$AnimationPlayer.play("flash")
+		else:
+			print("no flash animation")
+
+
+
+func _on_ReloadTimer_timeout():
+	shots_left = shots_per_burst
+	shoot()
+
+func _on_CockDurationTimer_timeout():
+	
+	shoot() # shoot method restarts the CockDurationTimer or ReloadTimer
+
+
+
+
+		
+			
+	
---
+++ scenes/Entities/Projectiles/ShotgunProjectile.gd
@@ -0,0 +1,109 @@
+"""
+Basic projectile
+
+spawned, then travels until it reaches target position or range
+
+It seems like inherited animation players can't have custom animations for children,
+so you can create a new AnimationPlayer with a custom explode animation to override the default.
+
+"""
+
+extends Area2D
+
+var mech : KinematicBody2D # whoever launched you
+var team : int
+var damage : float
+var damage_type : int # see Global.damage_types
+var projectile_range : float
+var line_of_sight : bool
+var target_location : Vector2
+var speed : float = 600.0
+var human_fudge_factor : float = 2.5
+var velocity : Vector2
+export var lifetime : int = 110
+
+enum States { FLYING, EXPLODING, DEAD }
+var State = States.FLYING
+
+signal hit
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func init(myMech, myDamage, damageType, projectileRange, lineOfSight, targetLocation):
+	mech = myMech
+	team = myMech.team
+	damage = myDamage
+	damage_type = damageType
+	projectile_range = projectileRange
+	line_of_sight = lineOfSight
+	target_location = targetLocation
+	
+	if mech.is_in_group("enemies"):
+		set_collision_layer_bit(3, true)
+	elif mech.is_human_player:
+		set_collision_layer_bit(2, true)
+		speed *= human_fudge_factor
+	set_collision_mask_bit(0, true)
+	set_collision_mask_bit(1, true)
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if State == States.FLYING:
+		velocity = Vector2.RIGHT.rotated(global_rotation) * speed
+		global_position += velocity * delta
+		lifetime -= 1 * delta
+		if lifetime <= 0:
+			call_deferred("queue_free")
+		
+
+
+func explode():
+#	$CollisionShape2D.set_scale($CollisionShape2D.get_scale()*3.0)
+	var possibleTargets = get_overlapping_bodies()
+	for body in possibleTargets:
+		if body.has_method("get_team") and body.team != team:
+			hurt(body)
+#		if has_node("AnimationPlayer") and get_node("AnimationPlayer").has_animation("explode"): # custom AnimationPlayer on scenes that inherit this scene.
+#			$AnimationPlayer.play("explode")
+#		else:
+#			$defaultAnimationPlayer.play("explode")
+	call_deferred("queue_free")
+	State = States.EXPLODING
+
+func hurt(body):
+	if body.has_method("_on_hit"):
+		#warning-ignore:RETURN_VALUE_DISCARDED
+		connect("hit", body, "_on_hit")
+		var impactVector = body.global_position - self.global_position
+		emit_signal("hit", damage, impactVector, damage_type)
+		disconnect("hit", body, "_on_hit")
+	
+
+func _on_Projectile_body_entered(body):
+	
+	if State == States.FLYING and line_of_sight:
+		if body.has_method("_on_hit") == false: # walls, etc
+			explode()
+		elif body.team != team:
+			explode()
+		else: # friendly fire
+			pass
+			#hurt(body)
+		
+	elif State == States.EXPLODING:
+		if is_instance_valid(body) and body.get("team") != team:
+			hurt(body)
+			
+			
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "explode":
+		call_deferred("queue_free")
+		
